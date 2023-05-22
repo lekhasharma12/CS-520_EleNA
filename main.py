f701caac01a8a5058a8dfb9de6e9c8ffe2c769d2
lekhasharma <lekhasharma012@gmail.com>
2023-05-21 21:19:34 -0400

    Changes for shortestpath api

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ elena.register_blueprint(blueprint, url_prefix='/elena')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    src = "147, Brittany Manor Drive, The Boulders, Mill Valley, Amherst, Hampshire County, Massachusetts, 01002"
-    # dest = "University of Massachusetts Amherst, Mullins Way, Hadley, Hampshire County, Massachusetts, 01003"
-    dest = "Puffers Pond, Amherst"
-    shortest_path(src, dest, 'min', 25, 'walk', "Amherst", "Massachusetts")
+    # src = "147, Brittany Manor Drive, The Boulders, Mill Valley, Amherst, Hampshire County, Massachusetts, 01002"
+    # # dest = "University of Massachusetts Amherst, Mullins Way, Hadley, Hampshire County, Massachusetts, 01003"
+    # dest = "Puffers Pond, Amherst"
+    # shortest_path(src, dest, 'min', 25, 'walk', "Amherst", "Massachusetts")
 
-    # # for server run
-    # elena.run(host='127.0.0.1', port=5000, debug=True)
+    # for server run
+    elena.run(host='127.0.0.1', port=5000, debug=True)
