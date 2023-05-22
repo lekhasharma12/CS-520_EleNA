74c6fa5998602f1ad863ac17e46422ba2477ea32
lekhasharma <lekhasharma012@gmail.com>
2023-05-22 19:29:57 -0400

    Code clean up

--- controllers/route_controller.py
+++ controllers/route_controller.py
@@ -24,12 +24,9 @@ def shortest_path():
 
     if error:
         print(error_msg)
-        # error_msg = {"error": error_msg}
         return jsonify(error_msg)
     else:
         route = Route(source, destination, elevation_type, percent_increase, mode, city, state)
-
-        # shortest_path = nx.shortest_path(route.graph, route.source_node, route.destination_node, weight='length')
 
         # get shortest path with given conditions using dijkstras and astar
         result_dijkstras = dijkstras_with_elevation(route.graph, route.source_node, route.destination_node, route.elevation_type, route.percent_increase, route.mode)
@@ -44,12 +41,6 @@ def shortest_path():
         else:
             result = result_astar if (max_astar - min_astar) > (max_dijkstras - min_dijkstras) else result_dijkstras
 
-        # route_map = ox.plot_route_folium(route.graph, shortest_path, color='#000000', opacity=0.5)
-        # route_map = ox.plot_route_folium(route.graph, result_dijkstras['path'], route_map=route_map, color='#0000ff', opacity=0.5)
-        # route_map = ox.plot_route_folium(route.graph, result_astar['path'], route_map=route_map, color='#ff0000', opacity=0.5)
-        # route_map.save('route.html')
-
-        # print(result)
         et = time.time()
         print("Time elapsed ", et-st)
         return jsonify(result)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.blueprint import blueprint
-from utils.utils import make_graph
-from controllers.route_controller import shortest_path
 
 elena = Flask(__name__)
 CORS(elena)
@@ -11,11 +9,6 @@ elena.register_blueprint(blueprint, url_prefix='/elena')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # src = "147, Brittany Manor Drive, The Boulders, Mill Valley, Amherst, Hampshire County, Massachusetts, 01002"
-    # # src = "115 Brittany Manor Drive, Amherst"
-    # # dest = "University of Massachusetts Amherst, Mullins Way, Hadley, Hampshire County, Massachusetts, 01003"
-    # dest = "650 N Pleasant Street, Amherst"
-    # shortest_path(src, dest, 'max', 25, 'walk', "Amherst", "Massachusetts")
 
     # for server run
     elena.run(host='127.0.0.1', port=5000, debug=True)
