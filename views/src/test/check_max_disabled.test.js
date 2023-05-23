d1186ea50d822d0135da7687fe6c3edb37f1980c
sairajesh368 <jainisairajesh@gmail.com>
2023-05-22 22:31:57 -0400

    Added test files to test Max button based on user selection

---
+++ views/src/test/check_max_disabled.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import Test from '../Test';
+import React from 'react';
+
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM(`
+  <!doctype html>
+  <html>
+    <head>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4k2WLIJibVJ8ZmDTHtalCRVcDbfkPepM&libraries=places"></script>
+      <script>console.log(Window.google)</script>
+    </head>
+    <body>
+    </body>
+  </html>`, {runScripts: "dangerously"});
+const { window } = jsdom;
+
+
+test('Check is Find Path button is disabled when the source and destination are empty', () => {
+
+    const testInstance = new Test();
+  
+    const mockState = { elevation: 0 };
+
+    // Rendering the Test component
+    render(<Test />);
+
+    Object.defineProperty(testInstance, 'state', {
+        get: jest.fn().mockReturnValue(mockState),
+    });
+
+    const maxButton = screen.getByText('Max');
+
+    // Get the updated variant
+    const updatedVariant = maxButton.getAttribute('variant');
+
+    // Assert the updated variant
+    expect(updatedVariant).toBe('outlined');
+
+    jest.restoreAllMocks();
+
+});
